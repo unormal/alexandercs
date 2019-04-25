9a42f002fdcd8c3f3ab8035a829f37c48674c078
Brian Bucklew <unormal@gmail.com>
2019-04-25 14:17:00 -0400

    2015 d1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,81 +13,30 @@ namespace ConsoleApp1
 
     class Program
     {
-        static Rect CreateRect(string str)
-        {
-            var r = new Rect();
-
-            str = str.Replace(" ", "");
-            str = str.Replace("#", "");
-
-            var parts = str.Split(new char[] { ',', 'x', ':', '@' });
-
-            r.id = int.Parse(parts[0]);
-            r.x = int.Parse(parts[1]);
-            r.y = int.Parse(parts[2]);
-            r.w = int.Parse(parts[3]);
-            r.h = int.Parse(parts[4]);
-
-            //map[r.x,r.y] = r.id;
-            return r;
-        }
         static void Main(string[] args)
         {
             // int, float, double, byte, string <- value types
             // everything else <- reference types
 
-            string[] input = File.ReadAllLines("input.txt");
+            string input = File.ReadAllText("input.txt");
 
+            int answer = 0;
 
-            List<Rect> rects = new List<Rect>();
-
-            foreach (string line in input) rects.Add(CreateRect(line));
-
-            string[,] map = new string[1000, 1000];
-
-            for (int x = 0; x < 1000; x++)
+            for (int i = 0; i<input.Length; i++)
             {
-                for (int y = 0; y < 1000; y++)
+                if (input[i]=='(')
                 {
-                    map[x, y] = ".";
+                    answer+=1;
+                } else if (input[i]==')')
+                {
+                    answer-=1;
                 }
-            }
-
-            HashSet<int> overlappingIds = new HashSet<int>();
-
-            foreach (Rect r in rects)
-            {
-                for (int x = r.x; x < r.x + r.w; x++)
+                if (answer==-1)
                 {
-                    for (int y = r.y; y < r.y + r.h; y++)
-                    {
-
-                        if (map[x, y] == ".")
-                        {
-                            map[x, y] = r.id.ToString();
-                        }
-                        else
-                        {
-                            overlappingIds.Add(r.id);
-
-                            if ( map[x,y] != "X") overlappingIds.Add( int.Parse(map[x,y]) );
-                            map[x,y] = "X";
-                    
-                        }
-                    }
-                }
-            }
-            int answer = -1;
-
-            for (int t = 1; t < 1302; t++)
-            {
-                if (!overlappingIds.Contains(t))
-                {
-                    answer = t;
+                    answer=i+1;
                     break;
                 }
             }
-
 
             Console.WriteLine();
             Console.WriteLine("Answer: " + answer);
