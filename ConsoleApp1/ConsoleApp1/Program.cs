0078eb657679f50551d6befef49c57ef6a322d05
Brian Bucklew <unormal@gmail.com>
2019-05-13 21:23:33 -0400

    2015 d1 p2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,78 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ConsoleApp1
 {
-    class Present : IComparable
+    class Event : IComparable
     {
-        public Present()
+        public DateTime time;
+        public string text;
+        public int guardid;
+
+        public Event(DateTime time, string text, int guardid)
         {
-            l = 0;
-            w = 0;
-            h = 0;
-        }
-
-        public Present( int startl, int startw, int starth )
-        {
-            l = startl;
-            w = startw;
-            h = starth;
-        }
-
-        public int l,w,h;
-
-        public int GetSurfaceArea()
-        {
-            return 2*l*w + 2*w*h + 2*h*l;
-        }
-        public int GetSmallestPerimeter()
-        {
-            var x = new List<int>();
-
-            x.Add(l + w + l + w);
-            x.Add(w + h + w + h);
-            x.Add(h + l + h + l);
-
-            x.Sort();
-
-            return x[0];
-        }
-
-        public int GetExtraPaperRequired()
-        {
-            var x = new List<int>();
-
-            x.Add(l * w);
-            x.Add(w * h);
-            x.Add(h * l);
-
-            x.Sort();
-
-            return x[0];
-        }
-        public int GetExtraRibbonRequired()
-        {
-            return w*h*l;
-        }
-        public int GetPaperRequired()
-        {
-            return GetSurfaceArea() + GetExtraPaperRequired();
-        }
-        public int GetRibbonRequired()
-        {
-            return GetSmallestPerimeter() + GetExtraRibbonRequired();
+            this.time = time;
+            this.text = text;
+            this.guardid = guardid;
         }
 
         public int CompareTo(object obj)
         {
-            Present p = obj as Present;
-            return GetSurfaceArea().CompareTo(p.GetSurfaceArea());        
+            var otherEvent = obj as Event;
+            return time.CompareTo(otherEvent.time);
         }
     }
-
     class Program
     {
+        static int GetGuardNumber(string str)
+        {
+            var splitted = str.Split('#');
+
+            return int.Parse(splitted[1].Split(' ')[0]);
+        }
         static void Main(string[] args)
         {
             // int, float, double, byte, string <- value types
@@ -80,37 +40,110 @@ namespace ConsoleApp1
 
             string[] input = File.ReadAllLines("input.txt");
 
-            var presents = new List<Present>();
-
-            foreach (string s in input)
-            {
-                string[] x = s.Split('x');
-                Present p = new Present(int.Parse(x[0]), int.Parse(x[1]), int.Parse(x[2]));
-
-                presents.Add(p);
-            }
             int answer = 0;
 
-            foreach (Present p in presents)
+            var events = new List<Event>();
+            var guards = new Dictionary<int,TimeSpan>();
+
+
+            foreach (string item in input)
             {
-                answer += p.GetPaperRequired();
+                string[] i = item.Split(']');
+                events.Add(new Event(DateTime.Parse(i[0].Substring(1)),i[1],-1));
+            }
+            events.Sort();
+            
+            int guardid = -1;
+            DateTime time = new DateTime();
+
+            TimeSpan amount = new TimeSpan(0);
+
+            foreach (Event ev in events)
+            {
+                if (ev.text.Contains("Guard"))
+                {
+                    guardid = GetGuardNumber(ev.text);
+                } else if (ev.text.Contains("falls"))
+                {
+                    time=ev.time;
+                } else if (ev.text.Contains("wakes"))
+                {
+                    amount += ev.time - time;
+
+                    if (!guards.ContainsKey(guardid))
+                    {
+                        guards.Add(guardid, amount);
+                    }
+                    else
+                    {
+                        guards[guardid] += amount;
+                    }
+                    amount = new TimeSpan(0);
+                }
+
+                ev.guardid = guardid;
+            }
+            
+            double longest = double.MinValue;
+            int longestMinute = -1;
+            int longestMinuteGuard = -1;
+            int longestGuard = -1;
+
+            foreach( var guard in guards )
+            {
+                if( guard.Value.TotalMinutes > longest )
+                {
+                    longest = guard.Value.TotalMinutes;
+                    longestGuard = guard.Key;
+                }
+            }
+
+            var listofguards = guards.Keys.ToList();
+
+            var minutecount = new Dictionary<int, int>(); // minute,times
+
+            int sleepminute = 0;
+
+            int longestminutecount = int.MinValue;
+
+            foreach( int currentguard in listofguards )
+            {
+                minutecount.Clear();
+                foreach (Event ev in events)
+                {
+                    if (ev.guardid == currentguard)
+                    {
+                        if (ev.text.Contains("falls"))
+                        {
+                            sleepminute=ev.time.Minute;
+                        } else if (ev.text.Contains("wakes"))
+                        {
+                            for (int i=sleepminute;i<ev.time.Minute;i++)
+                            {
+                                if (minutecount.ContainsKey(i))
+                                {
+                                    minutecount[i]+=1;
+                                } else
+                                {
+                                    minutecount.Add(i, 1);
+                                }
+                                if( minutecount[i] > longestminutecount )
+                                {
+                                    longestMinute=i;
+                                    longestMinuteGuard=currentguard;
+                                    longestminutecount=minutecount[i];
+                                }
+                            }
+                        }
+                    }
+                }
             }
 
 
+            answer = longestMinuteGuard*longestMinute;
 
             Console.WriteLine();
-            Console.WriteLine("Paper: " + answer);
-
-            answer = 0;
-
-            foreach (Present p in presents)
-            {
-                answer += p.GetRibbonRequired();
-            }
-
-            presents.Sort();
-
-            Console.WriteLine("Ribbon: " + answer);
+            Console.WriteLine(answer);
             Console.ReadKey();
         }
     }
