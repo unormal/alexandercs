d4419625f4ab3bd4531a004411c57b285fe5bd75
Brian Bucklew <unormal@gmail.com>
2019-05-27 18:26:17 -0400

    2017 d1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -21,46 +21,30 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string[] input = File.ReadAllLines("input.txt");
+            string oldinput = File.ReadAllText("input.txt");
             
-            var code = "";
+            var input = new List<int>();
 
-            int x=0;
-            int y=2;
-
-            string[,] pad = new string[5,5];
-            pad[0,0] = "X"; pad[1,0] = "X"; pad[2,0] = "1"; pad[3,0] = "X"; pad[4,0] = "X";
-            pad[0,1] = "X"; pad[1,1] = "2"; pad[2,1] = "3"; pad[3,1] = "4"; pad[4,1] = "X";
-            pad[0,2] = "5"; pad[1,2] = "6"; pad[2,2] = "7"; pad[3,2] = "8"; pad[4,2] = "9";
-            pad[0,3] = "X"; pad[1,3] = "A"; pad[2,3] = "B"; pad[3,3] = "C"; pad[4,3] = "X";
-            pad[0,4] = "X"; pad[1,4] = "X"; pad[2,4] = "D"; pad[3,4] = "X"; pad[4,4] = "X";
-
-
-            foreach (string str in input)
+            foreach (var item in oldinput)
             {
-                foreach (char dir in str)
-                {
-                    if (dir=='L' && pad[Math.Max(0,x-1),y] != "X" && x!=0)
-                    {
-                        x-=1;
-                    } else if (dir== 'R' && pad[Math.Min(4,x+1), y] != "X" && x!=4)
-                    {
-                        x+=1;
-                    } else if (dir=='U' && pad[x, Math.Max(0,y-1)] != "X" && y!=0)
-                    {
-                        y-=1;
-                    } else if (dir=='D' && pad[x, Math.Min(4,y+1)] != "X" && y!=4)
-                    {
-                        y+=1;
-                    }
-                }
-                code = code + pad[x,y];
+                input.Add(int.Parse(""+item));
             }
 
+            int sum = 0;
+            int half = input.Count/2;
+            int next;
 
+            for (int i=0;i<input.Count;i++)
+            {
+                next = input[(i+half)%input.Count];
+                if (input[i]==next)
+                {
+                    sum+=input[i];
+                }
+            }
 
             Console.WriteLine();
-            Console.WriteLine("answer: "+code);
+            Console.WriteLine("answer: "+sum);
             Console.ReadKey();
         }
     }
