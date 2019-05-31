95faf31a83547e3fe2ed8dcc7c2c3b6b9920b83c
Brian Bucklew <unormal@gmail.com>
2019-05-30 20:22:51 -0400

    2018 d3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -19,29 +19,57 @@ namespace ConsoleApp1
     }
     class Program
     {
+        public List<int> parseLine( string line )
+        {
+            //...
+            throw new NotImplementedException();
+        }
+
         static void Main(string[] args)
         {
-            string oldinput = File.ReadAllText("input.txt");
+            string[] input = File.ReadAllLines("input.txt");
             
-            var input = new List<int>();
+            var lines = new Dictionary<int,List<int>>();
+            var differences = new List<decimal>();
+            var even = new List<decimal>();
+            var alreadydid = new List<int>();
+            for (int i = 0;i<input.Length;i++) {
+                lines.Add(i,input[i].Replace('\t',' ').Split(' ').Select( e => int.Parse(e)).ToList());
+            }
 
-            foreach (var item in oldinput)
+            bool br=false;
+            for (int i = 0; i < lines.Count; i++)
             {
-                input.Add(int.Parse(""+item));
+                foreach (var item in lines[i])
+                {
+                    foreach (var check in lines[i])
+                    {
+                        if ((decimal)(item) / (decimal)(check) == Math.Floor((decimal)(item / check)) && item!=check && item/check!=0)
+                        {
+                            even.Add(item / check);
+                            alreadydid.Add(item / check);
+                            Console.WriteLine(item+","+check+"="+(item/check));
+                            br=true;
+                            break;
+                        }
+                    }
+                    if (br)
+                    {
+                        br=false;
+                        alreadydid.Clear();
+                        break;
+                    }
+                }
             }
 
             int sum = 0;
-            int half = input.Count/2;
-            int next;
-
-            for (int i=0;i<input.Count;i++)
+            foreach (int val in even)
             {
-                next = input[(i+half)%input.Count];
-                if (input[i]==next)
-                {
-                    sum+=input[i];
-                }
+                Console.WriteLine(val);
+                sum+=val;
             }
+
+            
 
             Console.WriteLine();
             Console.WriteLine("answer: "+sum);
